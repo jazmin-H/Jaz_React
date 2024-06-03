674744da73bbbf8194a7006783114e79a32ef7e0
jazmin-H <huaynocajazmin999@email.com>
2024-06-03 17:01:22 -0300

    empezamos tema nuevo que es react y hicimos lo basico

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import {MyPrimerSamira} from './components/miPrimerComponente/Mi1ercomp'
+
+const Myfunction = () => {
+  return<>hola </>;
+}
+//props
+function App() {
+
+  return (
+    <>
+       <Myfunction/>
+       <MyPrimerSamira name={'Samira'} last_name={'Chamilka'}/>
+    </>
+  );
+}
+
+export default App
---
+++ react/vite-project/src/components/miPrimerComponente/Mi1ercomp.jsx
@@ -0,0 +1,5 @@
+export function MyPrimerSamira({name, last_name}) {
+    return  (<> 
+     Mi nombre es {name} {last_name}
+    <img src="./imgs/shoto.jpg" alt=""/> </>)
+}
