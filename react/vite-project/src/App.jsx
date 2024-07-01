8100ee7fbc3b54102d972e2750760cc9f5d05cca
jazmin-H <huaynocajazmin999@email.com>
2024-07-01 16:56:34 -0300

    agregue el convertidor de monedas

--- react/vite-project/src/App.jsx
+++ react/vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import {MyPrimerSamira} from './components/miPrimerComponente/Mi1ercomp'
 import { Estado } from './components/estado/Estado'
 import { GetPost } from './components/GetPost/GetPost'
 import {Personajes} from './components/personajesRM/Personajes'
-
+import {ConvertidorMonedas} from './components/ConvertidorMoneda/Convmoneda'
 const Myfunction = () => {
   return<>hola </>;
 }
@@ -16,6 +16,7 @@ function App() {
        <Estado/>
        <GetPost/>
        <Personajes/>
+       <ConvertidorMonedas/>
     </>
   );
 }
